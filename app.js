dc13a12bbb8190db87f5a093cccd30da72232f0f
Sudip Mahato <mahatosudip837@gmail.com>
2023-07-05 14:25:26 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:false,
+        autoplay:true,
+        autoplayTimeout:1000,
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            },
+            1200:{
+                items:4
+            }
+
+        }
+    })
+});
+$(document).ready(function(){
+    $('#department').click(function(){
+        $('#fruit-name').toggle('fast');
+    });
+    $('#menu').click(function(){
+        $('nav').toggle('fast')
+    });
+});
+
+let search = document.querySelector('#search');
+let searchInput = document.querySelector('.input-section .input input');
+
+search.addEventListener('click',()=>{
+    searchInput.classList.toggle('search-input')
+})
+
+
+window.onscroll = ()=>{
+    $(document).ready(function(){
+        $('nav').hide('slow');
+    });
+}
